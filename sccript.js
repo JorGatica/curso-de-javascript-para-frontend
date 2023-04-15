0d3514384a6c3f9b781bcad116f9a959c9faefae
Jorge Gatica <mauriciogaticalopez@gmail.com>
2023-04-14 21:49:50 -0500

    agregando archivos

---
+++ sccript.js
@@ -0,0 +1,49 @@
+
+const h1 = document.querySelector('h1');
+const input1 = document.querySelector('#calculo1');
+const input2 = document.querySelector('#calculo2');
+const btn = document.querySelector('#btn-calcular');
+const pResult = document.querySelector('#result');
+const form=document.querySelector('#formulario');
+/*function btnOnClick(){
+    const valor1 = Number(input1.value);
+    const valor2 = Number(input2.value);
+    const resultado = valor1 + valor2;
+    h1.innerText = `Resultado: ${resultado}`;
+}*/
+
+/*function btnOnClick() {
+
+console.log(input1.value + input2.value);
+
+}*/
+
+/*function btnOnClick(){
+    const sumainputs = input1.value + input2.value;
+    pResult.innerText = "Resultado: " + sumainputs;
+}*/
+
+
+//addEventListener
+
+/*btn.addEventListener("click", btnOnClick);
+
+function btnOnClick(){
+
+    const valor1 = Number(input1.value);
+    const valor2 = Number(input2.value);
+    const resultado = valor1 + valor2;
+    pResult.innerText = `Resultado: ${resultado}`;
+
+}*/
+
+/*form.addEventListener('submit', sumarInputsValues);
+
+function sumarInputsValues(event){
+    //console.log({event});
+    event.preventDefault();
+    const valor1 = Number(input1.value);
+    const valor2 = Number(input2.value);
+    const resultado = valor1 + valor2;
+    pResult.innerText = `Resultado: ${resultado}`;
+}*/
